501e714175bd340de69a64fe15faf0c5a15fdadb
Sharan-SS <sharanvignesh123@gmail.com>
2021-10-07 06:56:22 +0530

    Converting a prefix exp to postfix

---
+++ Stack/Conversion/prefixToInfix.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+
+
+public class prefixToInfix {
+    static int operatorCheck(char x){
+        switch(x){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            
+                return 1;
+            default:
+                return 0;
+        }
+    }
+
+    public static String Convert(String str){
+        Stack<String> stack = new Stack<>();
+        
+        for(int i= str.length()-1;i>=0;i--){
+            char c = str.charAt(i);
+            if(operatorCheck(c)==1){
+                String opr1 = stack.pop();
+                String opr2 = stack.pop();
+
+                String temp = "("+opr1+c+opr2+")";
+                stack.push(temp);
+            }
+            else{
+                stack.push(c+"");
+            }
+
+        }
+        return stack.pop();
+
+    }
+
+    public static void main(String args[]){
+        String exp = "*-A/BC-/AKL";
+    System.out.println("Infix : " + Convert(exp));
+        
+    } 
+}
