cbfd93f5f549c08db77c079dec390c0685921b4a
Sharan-SS <sharanvignesh123@gmail.com>
2021-11-18 10:39:25 +0530

    Balanced Paranthesis

---
+++ Stack/Standard Problems/balancedPara.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    boolean checkBalanced(String str) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char check;
+                switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{') {
+                        return false;
+                    }
+                    break;
+                }
+            }
+
+        }
+        return (stack.isEmpty());
+    }
+}
+
+class balancedPara {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        String exp = "([{}]])";
+        System.out.println(obj.checkBalanced(exp));
+    }
+}
