eaec87079c6f2470387394e5d672b3f28f91a609
Sharan-SS <sharanvignesh123@gmail.com>
2021-12-03 20:34:14 +0530

    Added Minimum Binary Heap

--- Binary Tree/Traversals/BinHeap.java
+++ Binary Tree/Traversals/BinHeap.java
@@ -18,6 +18,25 @@ public class BinHeap {
         }
     }
 
+    static void minHeapify(int arr[], int n, int i) {
+        int left = (2 * i) + 1;
+        int right = (2 * i) + 2;
+        int largest = i;
+
+        if (left < n && arr[left] < arr[largest]) {
+            largest = left;
+        }
+        if (right < n && arr[right] < arr[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            maxHeapify(arr, n, largest);
+        }
+    }
+
     static void buildHeap(int arr[], int n) {
         int non_leaf = (n / 2) - 1;
         for (int i = non_leaf; i >= 0; i--) {
