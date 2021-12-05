bb722d8a7e8eb6daa0adf71456621d4ec79f801e
Sharan-SS <sharanvignesh123@gmail.com>
2021-12-05 21:40:15 +0530

    Added Utility Function for Searching

--- Binary Search Tree/insertion.java
+++ Binary Search Tree/insertion.java
@@ -1,10 +1,10 @@
-public class insertion{
+public class insertion {
 
-    class Node{
+    class Node {
         int data;
-        Node left,right;
+        Node left, right;
 
-        public Node(int item){
+        public Node(int item) {
             this.data = item;
             this.right = this.left = null;
         }
@@ -12,38 +12,59 @@ public class insertion{
 
     Node root;
 
-    insertion(){
+    insertion() {
         root = null;
     }
 
-    void insert(int key){
-        root = recursiveInsert(root,key);
+    void insert(int key) {
+        root = recursiveInsert(root, key);
     }
+
     Node recursiveInsert(Node root, int key) {
-        if(root == null){
+        if (root == null) {
             root = new Node(key);
             return root;
         }
 
-        if(root.data > key){
+        if (root.data > key) {
             root.left = recursiveInsert(root.left, key);
-        }
-        else if(root.data < key){
+        } else if (root.data < key) {
             root.right = recursiveInsert(root.right, key);
         }
         return root;
     }
-    void inOrderRecur(){
+
+    void inOrderRecur() {
         inOrder(root);
     }
-    void inOrder(Node root){
-        if(root != null){
+
+    void inOrder(Node root) {
+        if (root != null) {
             inOrder(root.left);
-            System.out.println(root.data+" ");
+            System.out.println(root.data + " ");
             inOrder(root.right);
         }
     }
 
+    public boolean search(int data) {
+      root =  searchBST(root, data);
+      if(root!=null){
+          return true;
+      }
+      else{
+          return false;
+      }
+    }
+
+    Node searchBST(Node root, int key) {
+        if (root == null || root.data == key) {
+            return root;
+        } else if (key < root.data) {
+            searchBST(root.left, key);
+        }
+
+        return searchBST(root.right, key);
+    }
 
     public static void main(String[] args) {
         insertion obj = new insertion();
@@ -55,5 +76,7 @@ public class insertion{
         obj.insert(60);
         obj.insert(80);
         obj.inOrderRecur();
+        // System.out.println(obj.search(40));
+
     }
 }
